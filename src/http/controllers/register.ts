4d94cae907b77753c055919192c4d7d675063bd6
BrenoFreitasM <brenofreitasmendes@gmail.com>
2025-11-11 00:43:33 -0300

    feat: adding email validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -12,7 +12,17 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = registerBodySchema.parse(request.body);
 
-    const password_hash = await hash(password, 6)
+    const password_hash = await hash(password, 6);
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if (userWithSameEmail) {
+        return reply.status(409).send({ message: 'Email já existente!'})
+    }
 
     await prisma.user.create({
         data: {

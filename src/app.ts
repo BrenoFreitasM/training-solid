ad4f0d900a0611877a869b20ca1882853959e810
BrenoFreitasM <brenofreitasmendes@gmail.com>
2026-03-03 02:36:52 -0300

    feat: global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from "fastify";
+import { env } from "./env";
 import { appRoute } from "./http/route";
+import { ZodError } from "zod";
 
 export const app = fastify()
 
 app.register(appRoute)
+
+app.setErrorHandler((error, request, reply) => {
+    if ( error instanceof ZodError) {
+        return reply
+        .status(400)
+        .send({ message: 'Validation error', issues: error.format() })
+    }
+
+    if (env.NODE_ENV !== 'production') {
+        console.log(error)
+    }
+
+    return reply.status(500).send({ message: 'Internal server error'})
+})

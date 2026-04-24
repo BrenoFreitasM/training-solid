cee37cad606ca24d1f945f212c492a9e97fb7d9a
BrenoFreitasM <brenofreitasmendes@gmail.com>
2026-04-24 14:32:55 -0300

    feat: adding factori methds

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,6 +1,7 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { AuthenticateUseCase } from "@/use-cases/authenticate";
 import { UserAlreadyExistsError } from "@/use-cases/errors/users-already-exists-error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from 'zod';
 
@@ -14,8 +15,8 @@ export async function authenticate (request: FastifyRequest, reply: FastifyReply
     const { email, password } = authenticateBodySchema.parse(request.body);
 
     try {
-        const usersRepository = new PrismaUsersRepository()
-        const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+        const authenticateUseCase = makeAuthenticateUseCase()
 
         await authenticateUseCase.execute({
             email,

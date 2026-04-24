f14cfb8db604987b610ea6cd328a456183f50ee3
BrenoFreitasM <brenofreitasmendes@gmail.com>
2026-04-23 23:01:06 -0300

    feat: authenticates controller

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,33 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { AuthenticateUseCase } from "@/use-cases/authenticate";
+import { UserAlreadyExistsError } from "@/use-cases/errors/users-already-exists-error";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from 'zod';
+
+export async function authenticate (request: FastifyRequest, reply: FastifyReply) {
+    const authenticateBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6)
+    })
+
+    const { email, password } = authenticateBodySchema.parse(request.body);
+
+    try {
+        const usersRepository = new PrismaUsersRepository()
+        const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+        await authenticateUseCase.execute({
+            email,
+            password
+        });
+    } catch (err) {
+        if (err instanceof UserAlreadyExistsError) {
+            return reply.status(400).send({ message: err.message })
+        }
+
+        throw err
+    }
+
+    return reply.status(200).send()
+}
